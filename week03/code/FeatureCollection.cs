a067bb6949695a2a67fc283df1dad87ca40821aa
Blake Torres <blaketorres2000@gmail.com>
2024-03-08 21:26:52 -0600

    Maze and Earthquake code

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,4 +1,18 @@
-public class FeatureCollection {
-    // Todo Earthquake Problem - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+// Feature.cs
+public class Feature
+{
+    public Properties Properties { get; set; }
+}
+
+// Properties.cs
+public class Properties
+{
+    public string Place { get; set; }
+    public double Mag { get; set; } // Renamed from Magnitude to match the JSON structure
+}
+
+// FeatureCollection.cs (rename if necessary)
+public class FeatureCollection
+{
+    public Feature[] Features { get; set; }
 }
--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -1,7 +1,9 @@
 using System.Text.Json;
 
-public static class SetsAndMapsTester {
-    public static void Run() {
+public static class SetsAndMapsTester
+{
+    public static void Run()
+    {
         // Problem 1: Find Pairs with Sets
         Console.WriteLine("\n=========== Finding Pairs TESTS ===========");
         DisplayPairs(new[] { "am", "at", "ma", "if", "fi" });
@@ -22,13 +24,15 @@ public static class SetsAndMapsTester {
         DisplayPairs(new[] { "ab", "aa", "ba" });
         // ba & ab
         Console.WriteLine("---------");
-        DisplayPairs(new[] { "23", "84", "49", "13", "32", "46", "91", "99", "94", "31", "57", "14" });
+        DisplayPairs(
+            new[] { "23", "84", "49", "13", "32", "46", "91", "99", "94", "31", "57", "14" }
+        );
         // 32 & 23
         // 94 & 49
         // 31 & 13
 
         // Problem 2: Degree Summary
-        // Sample Test Cases (may not be comprehensive) 
+        // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== Census TESTS ===========");
         Console.WriteLine(string.Join(", ", SummarizeDegrees("census.txt")));
         // Results may be in a different order:
@@ -38,7 +42,7 @@ public static class SetsAndMapsTester {
         // [5th-6th, 333], [10th, 933], [1st-4th, 168], [Preschool, 51], [12th, 433]}
 
         // Problem 3: Anagrams
-        // Sample Test Cases (may not be comprehensive) 
+        // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== Anagram TESTS ===========");
         Console.WriteLine(IsAnagram("CAT", "ACT")); // true
         Console.WriteLine(IsAnagram("DOG", "GOOD")); // false
@@ -77,7 +81,7 @@ public static class SetsAndMapsTester {
         maze.ShowStatus(); // Should be at (6,6)
 
         // Problem 5: Earthquake
-        // Sample Test Cases (may not be comprehensive) 
+        // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== Earthquake TESTS ===========");
         EarthquakeDailySummary();
 
@@ -91,16 +95,16 @@ public static class SetsAndMapsTester {
     }
 
     /// <summary>
-    /// The words parameter contains a list of two character 
-    /// words (lower case, no duplicates). Using sets, find an O(n) 
-    /// solution for displaying all symmetric pairs of words.  
+    /// The words parameter contains a list of two character
+    /// words (lower case, no duplicates). Using sets, find an O(n)
+    /// solution for displaying all symmetric pairs of words.
     ///
     /// For example, if <c>words</c> was: <c>[am, at, ma, if, fi]</c>, we would display:
     /// <code>
     /// am &amp; ma
     /// if &amp; fi
     /// </code>
-    /// The order of the display above does not matter. <c>at</c> would not 
+    /// The order of the display above does not matter. <c>at</c> would not
     /// be displayed because <c>ta</c> is not in the list of words.
     ///
     /// As a special case, if the letters are the same (example: 'aa') then
@@ -108,31 +112,68 @@ public static class SetsAndMapsTester {
     /// that there were no duplicates) and therefore should not be displayed.
     /// </summary>
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
-    private static void DisplayPairs(string[] words) {
-        // To display the pair correctly use something like:
-        // Console.WriteLine($"{word} & {pair}");
-        // Each pair of words should displayed on its own line.
+
+    //************************************************************************
+    // ****************  Display Pairs Code For Problem 1 ****************
+    //************************************************************************
+
+    private static void DisplayPairs(string[] strings)
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        foreach (string str in strings)
+        {
+            string reversedStr = ReverseString(str);
+
+            if (set.Contains(reversedStr))
+            {
+                Console.WriteLine($"{reversedStr} & {str}");
+            }
+            else
+            {
+                set.Add(str);
+            }
+        }
+    }
+
+    private static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
     }
 
     /// <summary>
     /// Read a census file and summarize the degrees (education)
     /// earned by those contained in the file.  The summary
     /// should be stored in a dictionary where the key is the
-    /// degree earned and the value is the number of people that 
+    /// degree earned and the value is the number of people that
     /// have earned that degree.  The degree information is in
     /// the 4th column of the file.  There is no header row in the
     /// file.
     /// </summary>
     /// <param name="filename">The name of the file to read</param>
     /// <returns>fixed array of divisors</returns>
-    /// #############
-    /// # Problem 2 #
-    /// #############
-    private static Dictionary<string, int> SummarizeDegrees(string filename) {
+
+    //************************************************************************
+    // ****************  Degree Dictionary Code For Problem 2 ****************
+    //************************************************************************
+
+    private static Dictionary<string, int> SummarizeDegrees(string filename)
+    {
         var degrees = new Dictionary<string, int>();
-        foreach (var line in File.ReadLines(filename)) {
+        foreach (var line in File.ReadLines(filename))
+        {
             var fields = line.Split(",");
-            // Todo Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3].Trim();
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
@@ -140,101 +181,136 @@ public static class SetsAndMapsTester {
 
     /// <summary>
     /// Determine if 'word1' and 'word2' are anagrams.  An anagram
-    /// is when the same letters in a word are re-organized into a 
+    /// is when the same letters in a word are re-organized into a
     /// new word.  A dictionary is used to solve the problem.
-    /// 
+    ///
     /// Examples:
     /// is_anagram("CAT","ACT") would return true
     /// is_anagram("DOG","GOOD") would return false because GOOD has 2 O's
-    /// 
+    ///
     /// Important Note: When determining if two words are anagrams, you
-    /// should ignore any spaces.  You should also ignore cases.  For 
+    /// should ignore any spaces.  You should also ignore cases.  For
     /// example, 'Ab' and 'Ba' should be considered anagrams
-    /// 
-    /// Reminder: You can access a letter by index in a string by 
+    ///
+    /// Reminder: You can access a letter by index in a string by
     /// using the [] notation.
     /// </summary>
-    /// #############
-    /// # Problem 3 #
-    /// #############
-    private static bool IsAnagram(string word1, string word2) {
-        // Todo Problem 3 - ADD YOUR CODE HERE
-        return false;
+
+    //************************************************************************
+    // ****************  Is It An Anagram Code For Problem 3 ****************
+    //************************************************************************
+
+    private static bool IsAnagram(string word1, string word2)
+    {
+        // Convert strings to lowercase and remove spaces and special characters
+        string cleanWord1 = new string(word1.ToLower().Where(char.IsLetter).ToArray());
+        string cleanWord2 = new string(word2.ToLower().Where(char.IsLetter).ToArray());
+
+        // Sort the characters of both strings
+        char[] sorted1 = cleanWord1.ToCharArray();
+        Array.Sort(sorted1);
+        char[] sorted2 = cleanWord2.ToCharArray();
+        Array.Sort(sorted2);
+
+        // Compare sorted strings
+        return new string(sorted1) == new string(sorted2);
     }
+
+    //************************************************************************
+    // ****************  Maze Code For Problem 4 *****************************
+    //************************************************************************
 
     /// <summary>
     /// Sets up the maze dictionary for problem 4
     /// </summary>
-    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap() {
-        Dictionary<ValueTuple<int, int>, bool[]> map = new() {
-            { (1, 1), new[] { false, true, false, true } },
-            { (1, 2), new[] { false, true, true, false } },
-            { (1, 3), new[] { false, false, false, false } },
-            { (1, 4), new[] { false, true, false, true } },
-            { (1, 5), new[] { false, false, true, true } },
-            { (1, 6), new[] { false, false, true, false } },
-            { (2, 1), new[] { true, false, false, true } },
-            { (2, 2), new[] { true, false, true, true } },
-            { (2, 3), new[] { false, false, true, true } },
-            { (2, 4), new[] { true, true, true, false } },
-            { (2, 5), new[] { false, false, false, false } },
-            { (2, 6), new[] { false, false, false, false } },
-            { (3, 1), new[] { false, false, false, false } },
-            { (3, 2), new[] { false, false, false, false } },
-            { (3, 3), new[] { false, false, false, false } },
-            { (3, 4), new[] { true, true, false, true } },
-            { (3, 5), new[] { false, false, true, true } },
-            { (3, 6), new[] { false, false, true, false } },
-            { (4, 1), new[] { false, true, false, false } },
-            { (4, 2), new[] { false, false, false, false } },
-            { (4, 3), new[] { false, true, false, true } },
-            { (4, 4), new[] { true, true, true, false } },
-            { (4, 5), new[] { false, false, false, false } },
-            { (4, 6), new[] { false, false, false, false } },
-            { (5, 1), new[] { true, true, false, true } },
-            { (5, 2), new[] { false, false, true, true } },
-            { (5, 3), new[] { true, true, true, true } },
-            { (5, 4), new[] { true, false, true, true } },
-            { (5, 5), new[] { false, false, true, true } },
-            { (5, 6), new[] { false, true, true, false } },
-            { (6, 1), new[] { true, false, false, false } },
-            { (6, 2), new[] { false, false, false, false } },
-            { (6, 3), new[] { true, false, false, false } },
-            { (6, 4), new[] { false, false, false, false } },
-            { (6, 5), new[] { false, false, false, false } },
-            { (6, 6), new[] { true, false, false, false } }
-        };
+    private static Dictionary<ValueTuple<int, int>, bool[]> SetupMazeMap()
+    {
+        Dictionary<ValueTuple<int, int>, bool[]> map =
+            new()
+            {
+                { (1, 1), new[] { false, true, false, true } },
+                { (1, 2), new[] { false, true, true, false } },
+                { (1, 3), new[] { false, false, false, false } },
+                { (1, 4), new[] { false, true, false, true } },
+                { (1, 5), new[] { false, false, true, true } },
+                { (1, 6), new[] { false, false, true, false } },
+                { (2, 1), new[] { true, false, false, true } },
+                { (2, 2), new[] { true, false, true, true } },
+                { (2, 3), new[] { false, false, true, true } },
+                { (2, 4), new[] { true, true, true, false } },
+                { (2, 5), new[] { false, false, false, false } },
+                { (2, 6), new[] { false, false, false, false } },
+                { (3, 1), new[] { false, false, false, false } },
+                { (3, 2), new[] { false, false, false, false } },
+                { (3, 3), new[] { false, false, false, false } },
+                { (3, 4), new[] { true, true, false, true } },
+                { (3, 5), new[] { false, false, true, true } },
+                { (3, 6), new[] { false, false, true, false } },
+                { (4, 1), new[] { false, true, false, false } },
+                { (4, 2), new[] { false, false, false, false } },
+                { (4, 3), new[] { false, true, false, true } },
+                { (4, 4), new[] { true, true, true, false } },
+                { (4, 5), new[] { false, false, false, false } },
+                { (4, 6), new[] { false, false, false, false } },
+                { (5, 1), new[] { true, true, false, true } },
+                { (5, 2), new[] { false, false, true, true } },
+                { (5, 3), new[] { true, true, true, true } },
+                { (5, 4), new[] { true, false, true, true } },
+                { (5, 5), new[] { false, false, true, true } },
+                { (5, 6), new[] { false, true, true, false } },
+                { (6, 1), new[] { true, false, false, false } },
+                { (6, 2), new[] { false, false, false, false } },
+                { (6, 3), new[] { true, false, false, false } },
+                { (6, 4), new[] { false, false, false, false } },
+                { (6, 5), new[] { false, false, false, false } },
+                { (6, 6), new[] { true, false, false, false } }
+            };
         return map;
     }
 
     /// <summary>
-    /// This function will read JSON (Javascript Object Notation) data from the 
+    /// This function will read JSON (Javascript Object Notation) data from the
     /// United States Geological Service (USGS) consisting of earthquake data.
     /// The data will include all earthquakes in the current day.
-    /// 
+    ///
     /// JSON data is organized into a dictionary. After reading the data using
     /// the built-in HTTP client library, this function will print out a list of all
     /// earthquake locations ('place' attribute) and magnitudes ('mag' attribute).
-    /// Additional information about the format of the JSON data can be found 
-    /// at this website:  
-    /// 
+    /// Additional information about the format of the JSON data can be found
+    /// at this website:
+    ///
     /// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-    /// 
+    ///
     /// </summary>
-    private static void EarthquakeDailySummary() {
-        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-        using var client = new HttpClient();
-        using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-        using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
-        using var reader = new StreamReader(jsonStream);
-        var json = reader.ReadToEnd();
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    private static void EarthquakeDailySummary()
+    {
+        try
+        {
+            const string uri =
+                "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+            using var client = new HttpClient();
+            using var getRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+            using var response = client.Send(getRequestMessage);
 
-        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+            // Log the HTTP status code
+            Console.WriteLine($"HTTP Status Code: {response.StatusCode}");
 
-        // TODO:
-        // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
-        // on those classes so that the call to Deserialize above works properly.
-        // 2. Add code below to print out each place a earthquake has happened today and its magitude.
+            using var jsonStream = response.Content.ReadAsStream();
+            using var reader = new StreamReader(jsonStream);
+            var json = reader.ReadToEnd();
+            Console.WriteLine($"JSON Response: {json}"); // Log the JSON response for debugging
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
+
+            foreach (var feature in featureCollection.Features)
+            {
+                Console.WriteLine($"{feature.Properties.Place} - Mag {feature.Properties.Mag}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 }
